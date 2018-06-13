baad6b03f08f1a1c680a78f5cc4bd73b38d67c34
Stanislav <shiptya@icloud.com>
2018-06-13 15:32:19 +0300

    ** Clear routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
+// Get basic api queries for mobile menu and main navigation device.
 Route::middleware('token')->get('beers', 'BeersController@index')->name('All Beers');
 Route::middleware('token')->get('posts', 'PostsController@index')->name('All Posts');
 Route::middleware('token')->get('dishes', 'DishesController@index')->name('Dishes in category');
 Route::middleware('token')->get('types', 'TypesController@index')->name('Categories menu');
 Route::middleware('token')->get('categories', 'TypesController@categories')->name('Categories bar');
 Route::middleware('token')->get('orders', 'OrdersController@index')->name('Shoping cart');
+Route::middleware('token')->post('preOrder', 'UsersController@preOrder')->name('preOrder table');
+Route::middleware('token')->get('history-orders', 'UsersController@history_orders')->name('history_orders');
+Route::middleware('token')->get('menu', 'UsersController@menu')->name('history_orders');
+// Get basic api queries for mobile menu and main navigation device.
 
 // Bonuses routes
 Route::middleware('token')->get('/bonuses/generate', 'BonusesController@generate_token')->name('generate_token');
@@ -20,31 +25,26 @@ Route::middleware('token')->get('notification/feedback', 'NotificationsControlle
 // Notifications routes
 
 // Orders routes
+Route::middleware('token')->post('beerga', 'BeersController@store')->name('Order Beer');
 Route::middleware('token')->post('order', 'OrdersController@store')->name('Order Dish');
 Route::middleware('token')->post('orders', 'OrdersController@order')->name('Order Dishes');
 Route::middleware('token')->post('bonuses/orders', 'OrdersController@order_bonuses')->name('Order bonuses Dishes');
 // Orders routes
 
-// Sidebar menus
-Route::middleware('token')->post('preOrder', 'UsersController@preOrder')->name('preOrder table');
-Route::middleware('token')->get('history-orders', 'UsersController@history_orders')->name('history_orders');
-Route::middleware('token')->get('menu', 'UsersController@menu')->name('history_orders');
-// Sidebar menus
-
-// Users routes
+// Users Auth and personal info
 Route::post('registration', 'UsersController@register')->name('register');
 Route::post('oauth', 'UsersController@login')->name('register');
 Route::post('restore_password', 'UsersController@ResetPassword')->name('ResetPassword');
 Route::post('social', 'UsersController@social')->name('social');
 Route::middleware('token')->post('logout', 'UsersController@logout')->name('logout');
+
 Route::middleware('token')->post('add_phone', 'UsersController@changePersonalPhone')->name('Change Personal Phone');
 Route::middleware('token')->post('profile/edit', 'UsersController@changePersonalInfo')->name('Change Personal Info');
 Route::middleware('token')->get('profile', 'UsersController@profile')->name('profile');
-// Users routes
+// Users Auth and personal info
 
 // Additional routes
 Route::middleware('token')->post('code', 'OrdersController@code')->name('Day code and count Tables');
-Route::middleware('token')->post('beerga', 'BeersController@store')->name('Order Beer');
 Route::middleware('token')->get('delivery', 'UsersController@delivery')->name('delivery_possible');
 Route::middleware('token')->post('devices', 'UsersController@devices')->name('devices');
 // Additional routes
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'BeergaController@welcome')->name('welcome');
 Route::get('/beers', 'BeersController@index')->name('getBeers');
 Route::get('/code',  'BeergaController@code')->name('getCode');
@@ -18,17 +7,11 @@ Route::get('/ticker', 'BeergaController@getRunningLine')->name('getTicker');
 
 Route::get('/notification/{notification}', 'NotificationsController@notificationPage')->name('notificationPageWeb');
 
-// Bonuses
-
-//get_bonuses
 Route::get('/bonuses/{bonusToken}', 'BonusesController@get_bonuses')->name('page-get-bonuses');
-
-//checkout
 Route::get('/bonuses/purchase/{bonusToken}', 'BonusesController@checkout')->name('page-purchase-bonuses');
 
 Route::post('/getbonuses', 'BonusesController@spendBonuses')->name('getbonuses');
 Route::post('/getOrders', 'BonusesController@obtainingBonuses')->name('obtainingBonuses');
-// Bonuses
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
